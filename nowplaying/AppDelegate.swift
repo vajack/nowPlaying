d41b75c57878fe045c9f9d20ec46757b96f283ca
Yuki-Ikegami <fp25657.hawks@icloud.com>
2018-06-04 21:22:57 +0900

    first commit

---
+++ nowplaying/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  nowplaying
+//
+//  Created by IkegamiYuki on 2018/06/04.
+//  Copyright © 2018 IkegamiYuki. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ nowplaying/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  nowplaying
+//
+//  Created by IkegamiYuki on 2018/06/04.
+//  Copyright © 2018 IkegamiYuki. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
